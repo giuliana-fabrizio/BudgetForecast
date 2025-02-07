930bbf3560e0201361646cd4db1e71305a80fea5
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-02-07 22:14:28 +0100

    feat: user can sign in

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+from controllers.auth_controllers import *
+
+app.secret_key = 'une clé (token) : grain de sel'
+app.register_blueprint(auth_controllers)
+
+@app.route('/')
+def authentification():
+    return render_template('login.html')
