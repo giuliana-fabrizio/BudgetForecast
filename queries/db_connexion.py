76dda25563c2173cb85bafa92dc8f334f856b217
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-02-07 22:18:12 +0100

    feat: add connexion with database

---
+++ queries/db_connexion.py
@@ -0,0 +1,16 @@
+from flask import g
+import sqlite3
+
+DATABASE = 'db/BudgetForecast.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
