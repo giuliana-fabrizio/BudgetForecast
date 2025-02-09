6756a20b41a2fe89d09cef954a7551fe4f73ac0e
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-02-09 22:45:02 +0100

    chore: minor change

--- queries/customer_queries.py
+++ queries/customer_queries.py
@@ -2,10 +2,7 @@ from queries.db_connexion import *
 
 def getUserByEmail(email):
     db = get_db()
-    return db.execute(
-        'select * from customer where mail like ?;',
-        [email]
-    ).fetchone()
+    return db.execute('select * from customer where mail like ?;', [email]).fetchone()
 
 def addUser(firstname, name, email, password):
     db = get_db()
