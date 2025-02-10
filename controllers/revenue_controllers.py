38f03f199122891c55811c1304357ba2e549d271
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-02-10 20:19:02 +0100

    feat: can add a revenue

--- controllers/revenue_controllers.py
+++ controllers/revenue_controllers.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, session, redirect, render_template, request, url_for
+from datetime import datetime
 from queries.db_connexion import *
 from queries.revenues_queries import *
 
@@ -11,3 +12,18 @@ def teardown_db(exception): close_db(exception)
 def displayRevenues():
     revenues = getRevenues()
     return render_template('/revenues/list.html', revenues=revenues)
+
+@revenue_controllers.route('/revenue/add', methods=['GET', 'POST'])
+def addRevenue():
+    if request.method == 'GET':
+        return render_template('/revenues/form.html', title="Ajouter un revenu")
+
+    name = request.form['name']
+    amount = request.form['amount']
+    created_at = datetime.now()
+    id_customer = session['id']
+
+    insertRevenue(name, amount, created_at, created_at, id_customer)
+
+    flash('Information: revenu ajouté avec succès', 'success')
+    return redirect(url_for('revenue_controllers.displayRevenues'))
--- queries/revenues_queries.py
+++ queries/revenues_queries.py
@@ -3,3 +3,12 @@ from queries.db_connexion import *
 def getRevenues():
     db = get_db()
     return db.execute('select * from revenue;').fetchall()
+
+def insertRevenue(name, amount, created_at, updated_at, id_customer):
+    db = get_db()
+    db.execute(
+        'insert into revenue(name, amount, created_at, updated_at, id_customer)\
+        values(?, ?, ?, ?, ?);',
+        [name, amount, created_at, updated_at, id_customer]
+    )
+    return db.commit()
